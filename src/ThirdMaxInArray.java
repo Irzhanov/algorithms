fcb14a608e350f4ff0d23c8951d9da99e922d082
Zhandos <zhIrzhanov@one.kz>
2021-03-12 22:25:26 +0600

    task(414): Third maximum number

--- src/ThirdMaxInArray.java
+++ src/ThirdMaxInArray.java
@@ -1,13 +1,34 @@
 public class ThirdMaxInArray {
 
-    public int thirdMax(int[] nums) {
-        int firstMax,secondMax,thirdMax;
+    public static void main(String[] args) {
+        int[] a = {5, 2, 8, 9, -1, 3};
+
+        System.out.println(thirdMax(a));
+    }
+
+    public static int thirdMax(int[] nums) {
+        Integer firstMax = null, secondMax = null,thirdMax= null;
         for(int i =0; i< nums.length; i++) {
-            thirdMax = nums[i];
-
+            if(firstMax == null || nums[i] > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = nums[i];
+            }
+            else if (nums[i] == firstMax)
+                continue;
+            else if(secondMax == null || nums[i] > secondMax) {
+                thirdMax = secondMax;
+                secondMax = nums[i];
+            }
+            else if (nums[i] == secondMax)
+                continue;
+            else if (thirdMax == null || nums[i] > thirdMax )
+                thirdMax = nums[i];
         }
-
-        return 0;
+        if(thirdMax != null)
+            return thirdMax;
+        else
+            return firstMax;
     }
 
 }
